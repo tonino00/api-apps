f5e6c3b5c78974682137cc13d9b926c2b2ebf150
toninosdev <antonino.gama@mam.bo>
2018-06-04 15:55:51 -0300

    ajustes novos

---
+++ eventos/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from eventos.models import Evento
+
+
+class EventoSerializer(ModelSerializer):
+    class Meta:
+        model = Evento
+        fields = ('__all__')
